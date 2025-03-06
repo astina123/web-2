67c01cadbaa2a4fb354bea93f2f7db11287fb0da
astina123 <aspanihsb@gmail.com>
2025-03-06 08:55:00 +0700

    Update and rename nilai_siswa.php to nilai_mahasiswa.php

--- nilai_mahasiswa.php
+++ nilai_mahasiswa.php
@@ -1,6 +1,6 @@
 <?php
 $proses = $_POST['proses'];
-$nama_siswa = $_POST['nama'];
+$nama_mahasiswa = $_POST['nama'];
 $mata_kuliah = $_POST['matkul'];
 $nilai_uts = $_POST['nilai_uts'];
 $nilai_uas = $_POST['nilai_uas'];
@@ -29,7 +29,7 @@ I = Tidak ada
 
 if (!empty($proses)) {
     echo 'Proses : ' . $proses;
-    echo '<br/>Nama : ' . $nama_siswa;
+    echo '<br/>Nama : ' . $nama_mahasiswa;
     echo '<br/>Mata Kuliah : ' . $mata_kuliah;
     echo '<br/>Nilai UTS : ' . $nilai_uts;
     echo '<br/>Nilai UAS : ' . $nilai_uas;
